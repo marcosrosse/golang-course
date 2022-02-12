59e49ce394d02edaf16144a018f448296d8157ed
Marcos Moreira <marcosmoreira@jsafrasarasin.com>
2022-02-12 00:52:04 -0300

    add http server example with routes and html templates

---
+++ 20-HTTP/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type user struct {
+	Name  string // Name is a field of the user struct and must be uppercase to be exported in the HTML template
+	Age   int8
+	Email string
+}
+
+var templates *template.Template
+
+func root(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Root Path!"))
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	u := user{
+		Name:  "John Wick", // Name is a field of the user struct and must be uppercase to be exported in the HTML template
+		Age:   50,
+		Email: "jhon@wick.com",
+	}
+	templates.ExecuteTemplate(w, "home.html", u) // ExecuteTemplate() executes the template associated with the given name in the set.
+}
+
+func users(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Load Users page!"))
+}
+
+func main() {
+
+	templates = template.Must(template.ParseGlob("www/*.html")) // ParseGlob() returns a template set containing all the templates matching the specified pattern.
+
+	http.HandleFunc("/", root)
+
+	http.HandleFunc("/home", home)
+
+	http.HandleFunc("/users", users)
+
+	fmt.Println("Server is listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
